a626af9cfecf13cb339c8d4cdca7ea4a6e94cd57
nayanr <nynrnjn@gmail.com>
2023-07-05 21:27:18 +0530

    addng files

---
+++ src/main/java/com/nayanr/statepattern/StatepatternApplication.java
@@ -0,0 +1,13 @@
+package com.nayanr.statepattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatepatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatepatternApplication.class, args);
+	}
+
+}
